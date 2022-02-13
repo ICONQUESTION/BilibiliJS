38fa710e60b2126488e33f67d1e58e87ddf6c8bc
ICONQUESTION <39425011+ICONQUESTION@users.noreply.github.com>
2022-02-13 13:15:15 +0800

    Add files via upload

---
+++ bilibiliLiveSign.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         bilibiliLiveSign
+// @namespace    https://iconquestion.github.io/
+// @version      0.1
+// @description  Hello!
+// @author       Microsoft365
+// @match        https://t.bilibili.com
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    fetch('https://api.live.bilibili.com/xlive/web-ucenter/v1/sign/DoSign',{
+        method:'get',
+        credentials:'include',
+        headers:{
+            Origin: 'https://link.bilibili.com',
+        }
+    });
+})();
