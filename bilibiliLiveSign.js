cc753be625de387e02866fd11b5ea7d3bbd5862a
Application <mojavenight@qq.com>
2022-07-17 12:57:33 +0800

    a

--- bilibiliLiveSign.js
+++ bilibiliLiveSign.js
@@ -1,21 +1,29 @@
 // ==UserScript==
 // @name         bilibiliLiveSign
 // @namespace    https://iconquestion.github.io/
-// @version      0.2
+// @version      0.51
 // @description  Hello!
 // @author       Microsoft365
-// @match        https://t.bilibili.com
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @match        https://t.bilibili.com/go
+// @icon         https://bilibili.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    fetch('https://api.live.bilibili.com/xlive/web-ucenter/v1/sign/DoSign',{
-        method:'get',
-        credentials:'include',
-        headers:{
-            Origin: 'https://link.bilibili.com',
+window.onload = function () {
+    console.log('正在签到')
+    fetch('https://api.live.bilibili.com/xlive/web-ucenter/v1/sign/DoSign', {
+        credentials: 'include',
+    }).then(function (res) {
+        return res.headers.get('Content-Type').search('application/json') != -1 ? res.json() : -1
+    }).then(function (data) {
+        //console.log(data)
+        if (data == -1) {
+            console.log('fetch 返回数据异常。')
+            return
         }
-    });
-})();
+
+        console.log(data.data ? data.data.text + '\n' + data.data.specialText : data.message)
+    })
+}
+
+
