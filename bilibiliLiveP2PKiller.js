3b9c85f5a4951d29717c698edb5f66027dbd52d3
Application <mojavenight@qq.com>
2025-02-01 12:28:55 +0800

    test

--- bilibiliLiveP2PKiller.js
+++ bilibiliLiveP2PKiller.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili LiveP2P Killer
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      2025-01-01
 // @description  Block Bilibili Livestreaming P2P Upload
 // @author       ICONQUESTION
 // @match        https://live.bilibili.com/*
